95816a362a6b7a9487bc1c75206da6ff3c4f08cb
kenneth cartel <kcartel@svengroup.com>
2020-11-10 00:11:40 +0800

    add connection

--- admin/connection.php
+++ admin/connection.php
@@ -11,6 +11,19 @@
 	// Check connection
 	if (!$conn) {
 	    die("Connection failed: " . mysqli_connect_error());
-    }
+	}
+	
+	// $servername = "localhost";
+	// $username = "u187511933_pj_monitoring";
+	// $password = "!Creatives2020";
+	// $database = "u187511933_pj_monitoring";
+
+	// // Create connection
+	// $conn = mysqli_connect($servername, $username, $password, $database);
+
+	// // Check connection
+	// if (!$conn) {
+	//     die("Connection failed: " . mysqli_connect_error());
+    // }
 
 ?> 
