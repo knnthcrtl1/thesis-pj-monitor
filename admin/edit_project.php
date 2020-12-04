51c402ab500b0074911a18b18c70a8672a38e2a3
kenneth cartel <kcartel@svengroup.com>
2020-12-04 17:36:06 +0800

    fix admin js

--- admin/edit_project.php
+++ admin/edit_project.php
@@ -798,8 +798,8 @@ include('./connection.php');
 
   <!-- Core plugin JavaScript-->
   <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-  <script src="admin/vendor/datatables/jquery.dataTables.min.js"></script>
-  <script src="admin/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+  <script src="vendor/datatables/jquery.dataTables.min.js"></script>
+  <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
 
   <script src="js/sb-admin-2.min.js"></script>
 
